baa54c4fed17aaf1539f36e6202ba724f2db4c86
Jason <j.meulenhoff@me.com>
2024-10-21 13:53:34 +0200

    Update to a newer event framework

--- RunLoop/IRunLoop.cs
+++ RunLoop/IRunLoop.cs
@@ -1,64 +1,14 @@
-using System;
-using System.Collections;
-using UnityEngine;
+﻿using System.Collections;
+using Framework.Events;
 
 namespace Framework.Loop
 {
     public interface IRunLoop
     {
-        /// <summary>
-        /// Wrapper for RepeatInvoke
-        /// </summary>
-        /// <param name="method"></param>
-        /// <param name="interval"></param>
-        public Coroutine StartRoutine(Action method, float interval);
+        public IEventListener<float> onUpdate { get; }
+        public IEventListener<float> onFixedUpdate { get; }
+        public IEventListener<float> onLateUpdate { get; }
 
-        /// <summary>
-        /// Starts a coroutine
-        /// </summary>
-        /// <param name="routine"></param>
-        public void CallCoroutine(IEnumerator routine);
-        
-        /// <summary>
-        /// Stops a coroutine
-        /// </summary>
-        /// <param name="routine"></param>
-        public void StopCoroutine(Coroutine routine);
-        
-        /// <summary>
-        /// Subscribe to runloop notifications
-        /// </summary>
-        /// <param name="callback"></param>
-        public void Subscribe(Action<float> callback);
-        
-        /// <summary>
-        /// Subscribe to runloop notifications
-        /// </summary>
-        /// <param name="callback"></param>
-        public void SubscribeFixed(Action<float> callback);
-                
-        /// <summary>
-        /// Subscribe to runloop notifications
-        /// </summary>
-        /// <param name="callback"></param>
-        public void SubscribeLate(Action<float> callback);
-        
-        /// <summary>
-        /// Unsubscribe from runloop notifications
-        /// </summary>
-        /// <param name="callback"></param>
-        public void UnSubscribe(Action<float> callback);
-
-        /// <summary>
-        /// Starts a routine
-        /// </summary>
-        /// <param name="routine"></param>
         public void Coroutine(IEnumerator routine);
-        
-        /// <summary>
-        /// Stops a routine
-        /// </summary>
-        /// <param name="routine"></param>
-        public void StopCoroutine(IEnumerator routine);
     }
 }
